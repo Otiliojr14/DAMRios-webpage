fa82c61363e9f206f602c7d791c35758b66b53a8
Otilio <otiliojr1999@gmail.com>
2021-11-21 10:04:23 -0600

    Esquema funcional completo

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,19 @@ $(function () {
         }, 350);
         
     });
+
+    $('.careers__container-mobile article').click(function () {
+        $(this).next("div.information-career").slideToggle().siblings("div.information-career").slideUp("slow");
+        var arrowIcon = $(this).find('.career__icon img');
+        if (arrowIcon.attr('src') === './assets/img/expand-arrow.png') {
+            $('.career__icon img').attr({src: "./assets/img/expand-arrow.png", alt:"icono expandir"});
+            $(this).find('.career__icon img').attr({src: "./assets/img/expand-arrow-open.png",
+  alt:"Icono expandir abierto"});						
+        } else {
+            $(this).find('.career__icon img').attr({src: "./assets/img/expand-arrow.png",
+  alt:"icono expandir"});
+        }
+    });
     
     
     
